94acd4f2b89ce92fb8c23b446c7d234ce6e2bc9f
SarahTruffert <77342348+SarahTruffert@users.noreply.github.com>
2021-02-25 16:38:54 +0100

    Update front.py
    PEP 8 ok

--- front.py
+++ front.py
@@ -13,18 +13,20 @@ def hello_world():
     #utilisé pour tester si l'app fonctionne bien
     return jsonify('Hello, World!')
 
+
 @app.route('/latest_by_country/<country>')
 def by_country(country):
     return jsonify(dico(country))
 
+
 @app.route('/average_by_year/<year>')
 def average_for_year(year):
     return jsonify(avg(year))
-     
+
+    
 @app.route('/per_capita/<country>')
 def per_capita(country):
     return jsonify(per_capi(country))
-    
 
 
 if __name__=="__main__":
