9549a875d23cd2c3cc1deb6fa493b8fc8c6acc5b
SarahTruffert <77342348+SarahTruffert@users.noreply.github.com>
2021-02-26 09:51:50 +0100

    Update front.py
    Logging ok

--- front.py
+++ front.py
@@ -1,31 +1,39 @@
 import json
 import logging
 import pandas as pd
+from back import dico, pays, avg, per_capi, ouvrir_fichier
 from flask import Flask, abort, jsonify
-from back import dico,pays,avg,per_capi,ouvrir_fichier
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+import logging
+# Arreter de ranger par ordre alphabétique
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(
+    filename="logging_api.log",
+    level=logging.ERROR,
+    format='%(asctime)s %(levelname)s - %(message)s',
+    datefmt='%d/%m/%Y %H:%M:%S',)
 
 
 @app.route('/')
 def hello_world():
     #utilisé pour tester si l'app fonctionne bien
+    app.logger.info(f"Hello World")
     return jsonify('Hello, World!')
-
 
 @app.route('/latest_by_country/<country>')
 def by_country(country):
+    app.logger.debug(f"Affiche dernière donnée pays")
     return jsonify(dico(country))
-
 
 @app.route('/average_by_year/<year>')
 def average_for_year(year):
+    app.logger.debug(f"Average")
     return jsonify(avg(year))
-
-    
+     
 @app.route('/per_capita/<country>')
 def per_capita(country):
+    app.logger.debug(f"calcul tons of carbon dioxide/ year/ country")
     return jsonify(per_capi(country))
 
 
