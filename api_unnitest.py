581336611976dd440bb0e0ed1a9d19a844b1fb39
SarahTruffert <77342348+SarahTruffert@users.noreply.github.com>
2021-03-01 10:54:12 +0100

    Update and rename apitest.py to api_unnitest.py

--- api_unnitest.py
+++ api_unnitest.py
@@ -1,50 +1,66 @@
 import unittest
-from front import app
+from fichier_api import app
 
 class TestApiFlask(unittest.TestCase):
 
-#Tester l'API
 
-    #Lance mon app 
     def setUp(self):
+        """ def setup run app
+        """
         self.app = app.test_client()
         self.app.testing = True 
 
-#Fait le chemin jusqu'a "/"
+
     def test_home_status_code(self):
+        """ def status_code test if path is ok "/"
+         """
         result = self.app.get('/') 
         self.assertEqual(result.status_code, 200) 
         # ".status_code = renvoie, si marche = 200 (ok)
 
+
     def test_home_data(self):
+        """ def data test data "Hello word" 
+        """
         result = self.app.get('/')
         # renvoit data
         self.assertEqual(result.data, b'"Hello, World!"\n')
 
-# tester si c'est bien une app j.son : content_type
+
     def test_home_type(self):
+        """ test_home_type test if te app is j.son content
+         """
         result = self.app.get('/')
         self.assertEqual(result.content_type,'application/json')
 
 
-# tester si le mot dedans, est il dedans ? OUI
     def test_home_word(self):
+        """ Test if the word is includes 
+        """
         result = self.app.get('/')
         self.assertTrue(result.data,'Hello')
 
-#Tester les fonctions : 
+
+        """ Test les fonctions @app: 
+        """
 
 #def by_country(country):
     def test_home_by_country(self):
+        """by_country test si country figure dans la requête
+        """
         result = self.app.get('/latest_by_country/Albania')
         self.assertTrue(b'country'in result.data)
 
 # fonction average :
+        """ test average by year test si year figure dans la requête
+        """
     def test_average_by_year_data(self):
         result = self.app.get("/average_by_year/1975")
         self.assertTrue(b'year' in result.data)
      
 # fonction ('/per_capita/<country>')
+        """ Test per_capita si year ne figure pas dans la 
+        """
     def test_per_capita(self):
         result = self.app.get('/per_capita/Algeria')
         self.assertFalse(b'year' in result.data)
